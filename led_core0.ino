82456eefb791ab3da7ef2b47d238eaffb1506e73
AnggaRagazorr <anggororedi@gmail.com>
2025-11-10 11:50:26 +0700

    Update led_core0.ino

--- led_core0.ino
+++ led_core0.ino
@@ -1,1 +1,16 @@
+#define LED_PIN 15
+
+void taskLED(void *pvParameters) {
+  while (1) {
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    delay(500);
+  }
+}
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  xTaskCreatePinnedToCore(taskLED, "LED_Task", 1000, NULL, 1, NULL, 0); 
+  
+}
+void loop() {}
 
