83f59c2549e0437a55aa89bcccd9096f41b1d4aa
AnggaRagazorr <anggororedi@gmail.com>
2025-11-10 13:15:21 +0700

    Update led_test.ino

--- LED/led_test.ino
+++ LED/led_test.ino
@@ -3,17 +3,21 @@
 void taskLED(void *pvParameters) {
   while (1) {
     digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    Serial.print("Task LED running on Core: ");
+    Serial.println(xPortGetCoreID());
     delay(500);
   }
 }
 
 void setup() {
+  Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
-  //core 0
-  xTaskCreatePinnedToCore(taskLED, "LED_Task", 1000, NULL, 1, NULL, 0); 
-  //core 1
-  //xTaskCreatePinnedToCore(taskLED, "LED_Task", 1000, NULL, 1, NULL, 1); 
-  
-}
-void loop() {}
 
+  // Core 0
+  xTaskCreatePinnedToCore(taskLED, "taskLED_Core0", 1000, NULL, 1, NULL, 0);
+
+  // Core 1
+  // xTaskCreatePinnedToCore(taskLED, "taskLED_Core1", 1000, NULL, 1, NULL, 1);
+}
+
+void loop() {}
